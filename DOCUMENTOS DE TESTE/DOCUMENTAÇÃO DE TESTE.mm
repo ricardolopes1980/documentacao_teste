c62b75fcb7434d4e21b0f96d0ccc2f2e93ce9291
ricardolopes1980 <92340724+ricardolopes1980@users.noreply.github.com>
2022-04-05 23:09:20 -0300

    Add files via upload
    Segue o link da estória do usuário dentro do Jira.
    
    https://papodeqa.atlassian.net/browse/VI133-11

---
+++ DOCUMENTOS DE TESTE/DOCUMENTAÇÃO DE TESTE.mm
@@ -0,0 +1,90 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1649200983035" ID="ID_812854960" MODIFIED="1649210269722" TEXT="DOCUMENTANDO O TESTE">
+<node CREATED="1649201128537" FOLDED="true" HGAP="24" ID="ID_108770497" MODIFIED="1649209207004" POSITION="right" TEXT="Evid&#xea;ncias de testes" VSHIFT="-83">
+<node CREATED="1649209188915" ID="ID_1425308153" MODIFIED="1649209194773">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="evidencia.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1649201087699" FOLDED="true" ID="ID_1066805217" MODIFIED="1649210272720" POSITION="right" TEXT="Desenvolvendo o olho de testador">
+<node CREATED="1649202772261" FOLDED="true" HGAP="32" ID="ID_1244314955" MODIFIED="1649209245659" TEXT="Leia a Est&#xf3;ria do usu&#xe1;rio ou o Requisito com aten&#xe7;&#xe3;o!" VSHIFT="-42">
+<node CREATED="1649209219298" ID="ID_1597244615" MODIFIED="1649209242507">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="estoria.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1649202821691" HGAP="41" ID="ID_343426919" MODIFIED="1649209123537" TEXT="Crie o Roteiro em Gherkin (BDD)" VSHIFT="-39">
+<node CREATED="1649202159759" HGAP="43" ID="ID_381966761" MODIFIED="1649202994403" TEXT="Descreva o caminho feliz" VSHIFT="-31">
+<node CREATED="1649202179984" HGAP="43" ID="ID_987576680" MODIFIED="1649202951397" TEXT="Enxergue caminhos alternativos para o teste" VSHIFT="-22"/>
+</node>
+</node>
+<node CREATED="1649202229792" HGAP="46" ID="ID_1364272945" MODIFIED="1649203000353" TEXT="Teste no Chrome" VSHIFT="8"/>
+<node CREATED="1649202332503" HGAP="49" ID="ID_241180125" MODIFIED="1649203184775" TEXT="Teste no Firefox" VSHIFT="58"/>
+</node>
+<node CREATED="1649201896470" FOLDED="true" HGAP="21" ID="ID_710373451" MODIFIED="1649210274789" POSITION="right" TEXT="A import&#xe2;ncia de transmitir as informa&#xe7;&#xf5;es com assertividade" VSHIFT="74">
+<node CREATED="1649201995864" ID="ID_1441532317" MODIFIED="1649202430470" TEXT="Defeito (BUG)">
+<node CREATED="1649202390776" ID="ID_1283184380" MODIFIED="1649202398484" TEXT="Busque identificar a origem do defeito"/>
+<node CREATED="1649202439056" HGAP="26" ID="ID_1370700346" MODIFIED="1649202456657" TEXT="Reproduza o defeito mais de uma vez" VSHIFT="25"/>
+</node>
+<node CREATED="1649202013039" HGAP="23" ID="ID_1033333488" MODIFIED="1649202475049" TEXT="Adequa&#xe7;&#xe3;o" VSHIFT="21"/>
+<node CREATED="1649202023401" HGAP="31" ID="ID_1662842480" MODIFIED="1649202030391" TEXT="Corre&#xe7;&#xe3;o" VSHIFT="51"/>
+<node CREATED="1649202484279" HGAP="32" ID="ID_668464921" MODIFIED="1649202494825" TEXT="Seja sucinto e objetivo" VSHIFT="34"/>
+</node>
+<node CREATED="1649201269711" FOLDED="true" HGAP="16" ID="ID_1281339270" MODIFIED="1649202143965" POSITION="right" TEXT="Ferramentas" VSHIFT="80">
+<node CREATED="1649201280197" FOLDED="true" HGAP="47" ID="ID_1263330420" MODIFIED="1649202140310" TEXT="Screen Recorder para Chrome" VSHIFT="-13">
+<node CREATED="1649201670003" ID="ID_183613401" MODIFIED="1649201677277">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="logo_screenrecorder.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1649201307698" FOLDED="true" HGAP="48" ID="ID_469617687" MODIFIED="1649202141371" TEXT="XRecorder para Mobile" VSHIFT="61">
+<node CREATED="1649201819633" HGAP="67" ID="ID_1354024712" MODIFIED="1649201837327" VSHIFT="1">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="logo_xrecorder.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1649201332269" FOLDED="true" HGAP="45" ID="ID_1834686217" MODIFIED="1649202142296" TEXT="Lightshot para captura de telas" VSHIFT="82">
+<node CREATED="1649201561600" HGAP="25" ID="ID_1563261331" MODIFIED="1649201592132" VSHIFT="23">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="logo_lightshot.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</map>
